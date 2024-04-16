502b8b1ea594c501d5c9a4b6359ab0de145273fb
huntermahmud <mahmudthedatahunter@gmail.com>
2024-04-16 19:59:28 +0600

    image gallery added

--- src/components/SmartHourseGallery.jsx
+++ src/components/SmartHourseGallery.jsx
@@ -9,20 +9,22 @@ import 'swiper/css/pagination';
 
 // import required modules
 import { Pagination, Autoplay } from 'swiper/modules';
+import { Link } from "react-router-dom";
 
 const SmartHourseGallery = () => {
   return (
     <div className="mx-2">
-      <div className="max-w-7xl mx-auto">
-        <div>
+      <div className="max-w-7xl mx-auto flex flex-col lg:flex-row text-center lg:text-left">
+        <div className="p-3">
           <h2 className="text-4xl">Introducing a New Smart House</h2>
-          <p className="text-lg">
+          <p className="text-lg mt-8">
             You are welcome to check out our house gallery to make sure that all
             apartments are in prime condition which makes them a perfect living
             space.
           </p>
+          <Link to='/properties' className="btn btn-primary bg-violet-500 mt-5">Learn More</Link>
         </div>
-        <div>
+        <div className="lg:mx-3">
           <Swiper
             slidesPerView={"auto"}
             centeredSlides={true}
@@ -34,38 +36,38 @@ const SmartHourseGallery = () => {
             className="mySwiper"
             autoplay={true}
           >
-            <SwiperSlide className="w-[300px]">
-             <img className="w-[300px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
+            <SwiperSlide>
+             <img className="w-full h-[300px] sm:h-[400px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
              
             </SwiperSlide>
-            <SwiperSlide className="w-[300px]">
-             <img className="w-[300px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
+            <SwiperSlide >
+             <img className="w-full h-[300px] sm:h-[400px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
              
             </SwiperSlide>
-            <SwiperSlide className="w-[300px]">
-             <img className="w-[300px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
+            <SwiperSlide className="">
+             <img className="w-full h-[300px] sm:h-[400px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
              
             </SwiperSlide>
-            <SwiperSlide className="w-[300px]">
-             <img className="w-[300px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
+            <SwiperSlide className="">
+             <img className="w-full h-[300px] sm:h-[400px]" src="https://source.unsplash.com/white-kitchen-room-set-G7sE2S4Lab4" alt="" />
              
             </SwiperSlide>
 
             <SwiperSlide>
               {" "}
-              <div className="w-[300px] h-[350px] sm:h-[500px] object-cover bg-cover bg-[url('https://source.unsplash.com/round-brown-wooden-table-with-six-white-leather-padded-chairs-Mx_W2atEaDo')] bg-center bg-no-repeat"></div>
+              <div className="w-full h-[300px] sm:h-[400px] object-cover bg-cover bg-[url('https://source.unsplash.com/round-brown-wooden-table-with-six-white-leather-padded-chairs-Mx_W2atEaDo')] bg-center bg-no-repeat"></div>
             </SwiperSlide>
             <SwiperSlide>
               {" "}
-              <div className="w-[300px] h-[350px] sm:h-[500px] object-cover bg-cover bg-[url('https://source.unsplash.com/a-living-room-with-a-large-window-fobX0HI9vVo')] bg-center bg-no-repeat"></div>
+              <div className="w-full h-[300px] sm:h-[400px] object-cover bg-cover bg-[url('https://source.unsplash.com/a-living-room-with-a-large-window-fobX0HI9vVo')] bg-center bg-no-repeat"></div>
             </SwiperSlide>
             <SwiperSlide>
               {" "}
-              <div className="w-[300px] h-[350px] sm:h-[500px] object-cover bg-cover bg-[url('https://source.unsplash.com/two-green-and-white-pillows-on-empty-bed-w1RE0lBbREo')] bg-center bg-no-repeat"></div>
+              <div className="w-full h-[300px] sm:h-[400px] object-cover bg-cover bg-[url('https://source.unsplash.com/two-green-and-white-pillows-on-empty-bed-w1RE0lBbREo')] bg-center bg-no-repeat"></div>
             </SwiperSlide>
             <SwiperSlide>
               {" "}
-              <div className="w-[300px] h-[350px] sm:h-[500px] object-cover bg-cover bg-[url('https://source.unsplash.com/brown-and-grey-room-interior-LL9V0iMpStM')] bg-center bg-no-repeat"></div>
+              <div className="w-full h-[300px] sm:h-[400px] object-cover bg-cover bg-[url('https://source.unsplash.com/brown-and-grey-room-interior-LL9V0iMpStM')] bg-center bg-no-repeat"></div>
             </SwiperSlide>
           </Swiper>
         </div>
